21db81dc803ba7b1f873b2485e0026a630731b10
ibragimovgalib <galibiavi4@gmail.com>
2023-03-28 15:13:22 +0300

    commm

--- test.js
+++ test.js
@@ -5,12 +5,11 @@
 
 
 // let date = new Date(2023,2,28,13,30)
-let date = new Date('2022-03-28 6:53:01')
+let date1 = new Date('2023-03-27 6:53:01')
+let date2 = new Date('2023-03-28 7:55:33')
 
-date.setFullYear(2033)
-date.setMonth(6)
-date.setDate(18)
-date.setHours(5)
-date.setMinutes(44)
-
-console.log(date);
+function rezDate(firstDate, lastDate) {
+    let rez = (lastDate.getTime()-firstDate.getTime())
+    return rez / (24*60*60*1000*)
+}
+console.log(rezDate(date1, date2));
