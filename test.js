539da6db3bcbd46ed8024d5c4ea6e7f40abed4f1
ibragimovgalib <galibiavi4@gmail.com>
2023-03-28 07:05:49 +0300

    first commit

---
+++ test.js
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+// let date = new Date(2023,2,28,13,30)
+let date = new Date('2022-03-28 6:53:01')
+
+date.setFullYear(2033)
+date.setMonth(6)
+date.setDate(18)
+date.setHours(5)
+date.setMinutes(44)
+
+console.log(date);
